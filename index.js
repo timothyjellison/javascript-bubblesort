bba356fbe5908b2092bf036e63bda2f12f70fedb
Tim Ellison <timothyellison85@gmail.com>
2018-09-06 16:09:38 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+function bubbleSort(array) {
+  let swapsMade = false;
+  
+  array.forEach(function(element, index) {
+    if (index <= array.length - 2 && number > array[index + 1]) {
+      let temp = number;
+      array[index] = array[index + 1];
+      array[index + 1] = temp;
+    }
+  }
+  
+  if (swapsMade) return bubbleSort(array);
+  
+  return array;
+}
